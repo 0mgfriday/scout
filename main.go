f781a4b1bf7848753a7d0f7cd72c0632479f7de3
0mgfriday <100394531+0mgfriday@users.noreply.github.com>
2024-04-06 14:08:27 -0400

    report from response start

--- main.go
+++ main.go
@@ -4,18 +4,18 @@ import (
 	"crypto/x509"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"os"
+	"regexp"
 	"strings"
 	"time"
 )
 
-const LabelColor = "\033[1;34m%s\033[0m"
-
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Println("Usage: certdump https://example.com")
+		fmt.Println("Usage: scout https://example.com")
 		os.Exit(0)
 	}
 
@@ -36,9 +36,9 @@ func main() {
 		panic(err)
 	}
 
-	tlsInfo := infoFromCert(resp.TLS.PeerCertificates[0])
+	result := reportFromResponse(resp)
 
-	j, err := json.MarshalIndent(tlsInfo, "", "    ")
+	j, err := json.MarshalIndent(result, "", "    ")
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -70,11 +70,81 @@ func infoFromCert(cert *x509.Certificate) *TLSInfo {
 
 type Report struct {
 	Url     string
-	Port    int
+	IP      string
 	TLS     TLSInfo
-	Title   string
 	Status  int
+	Title   string
 	Tech    string
 	Headers map[string]string
 	JSFiles []string
+}
+
+func reportFromResponse(rsp *http.Response) *Report {
+	report := Report{
+		Url:    rsp.Request.RequestURI,
+		IP:     rsp.Request.RemoteAddr,
+		Status: rsp.StatusCode,
+	}
+
+	report.TLS = *infoFromCert(rsp.TLS.PeerCertificates[0])
+	report.Headers = headersToMap(rsp.Header)
+
+	body := readAsString(rsp)
+	if body != "" {
+		report.Title = getTitle(body)
+		report.JSFiles = getJSFiles(body)
+	}
+
+	return &report
+}
+
+func headersToMap(header http.Header) map[string]string {
+	headers := make(map[string]string)
+	for name, values := range header {
+		for _, val := range values {
+			headers[name] = val // just replace if multiple for now
+		}
+	}
+
+	return headers
+}
+
+func readAsString(rsp *http.Response) string {
+	defer rsp.Body.Close()
+	bodyString := ""
+
+	if rsp.StatusCode == http.StatusOK {
+		bodyBytes, err := io.ReadAll(rsp.Body)
+		if err != nil {
+			fmt.Println(err)
+		}
+		bodyString = string(bodyBytes)
+	}
+
+	return bodyString
+}
+
+func getTitle(s string) string {
+	r := regexp.MustCompile(`<(title|Title|TITLE)>(?P<Title>.{1,150})</(title|Title|TITLE)>`)
+	match := r.FindStringSubmatch(s)
+	index := r.SubexpIndex("Title")
+	if index == -1 {
+		return ""
+	}
+
+	return match[index]
+}
+
+func getJSFiles(s string) []string {
+	files := []string{}
+	r := regexp.MustCompile(`<script src="(?P<Src>.{1,200})"`)
+	matches := r.FindAllStringSubmatch(s, -1)
+	for _, match := range matches {
+		index := r.SubexpIndex("Src")
+		if index != -1 {
+			files = append(files, match[index])
+		}
+	}
+
+	return files
 }
