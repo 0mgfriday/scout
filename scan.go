84da63069155b09b696e8fc16e7cbfdd719d8c12
0mgfriday <100394531+0mgfriday@users.noreply.github.com>
2024-04-13 12:16:57 -0400

    update impersonation info

--- scan.go
+++ scan.go
@@ -105,10 +105,10 @@ func (scan scanner) createReq(url string) (*http.Request, error) {
 	}
 
 	if scan.impersonateBrowser {
-		req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0")
-		req.Header.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
-		req.Header.Add("Accept-Language", "en-US,en;q=0.5")
-		req.Header.Add("Accept-Encoding", "gzip, deflate, br")
+		req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")
+		req.Header.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+		req.Header.Add("Accept-Language", "en-US,en;q=0.9")
+		req.Header.Add("Accept-Encoding", "gzip, deflate, br, zstd")
 		req.Header.Add("Upgrade-Insecure-Requests", "1")
 		req.Header.Add("Sec-Fetch-Dest", "document")
 		req.Header.Add("Sec-Fetch-Mode", "navigate")
