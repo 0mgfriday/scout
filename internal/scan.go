9b905a3cfc885b4087daa778292f8b34239bbb67
0mgfriday <100394531+0mgfriday@users.noreply.github.com>
2024-09-04 08:50:38 -0400

    probe for cors with origin

--- internal/scan.go
+++ internal/scan.go
@@ -13,9 +13,10 @@ import (
 type Scanner struct {
 	client             http.Client
 	impersonateBrowser bool
+	checkCORS          bool
 }
 
-func NewScanner(timeout int, impersonateBrowser bool, proxy string) (*Scanner, error) {
+func NewScanner(timeout int, impersonateBrowser bool, proxy string, checkCORS bool) (*Scanner, error) {
 	newScanner := Scanner{
 		client: http.Client{
 			CheckRedirect: func(req *http.Request, via []*http.Request) error {
@@ -30,6 +31,7 @@ func NewScanner(timeout int, impersonateBrowser bool, proxy string) (*Scanner, e
 			},
 		},
 		impersonateBrowser: impersonateBrowser,
+		checkCORS:          checkCORS,
 	}
 
 	if proxy != "" {
@@ -105,7 +107,7 @@ func (scan Scanner) createReq(url string) (*http.Request, error) {
 	}
 
 	if scan.impersonateBrowser {
-		req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")
+		req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36")
 		req.Header.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
 		req.Header.Add("Accept-Language", "en-US,en;q=0.9")
 		req.Header.Add("Upgrade-Insecure-Requests", "1")
@@ -115,6 +117,10 @@ func (scan Scanner) createReq(url string) (*http.Request, error) {
 		req.Header.Add("Sec-Fetch-User", "?1")
 	} else {
 		req.Header.Add("Accept", "*/*")
+	}
+
+	if scan.checkCORS {
+		req.Header.Add("Origin", "https://example.com")
 	}
 
 	return req, nil
